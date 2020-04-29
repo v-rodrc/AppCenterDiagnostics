a7d26986026e008770147e1d53a2e16b4f051c87
Christopher Rodriguez <christophererodriguez70@gmail.com>
2020-04-29 14:11:09 -0500

    Initial Commit

---
+++ AppCenterDiagnostics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppCenterDiagnostics
+//
+//  Created by Christopher Rodriguez on 4/29/20.
+//  Copyright © 2020 Christopher Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
